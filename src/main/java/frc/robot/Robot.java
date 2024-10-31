dd8738af01aa4eff7df5fb6eaa5a31a808376e2b
Abhigyan Mishra <mishraabhigyan009@gmail.com>
2024-10-30 23:18:04 -0500

    I typed the code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,7 +4,10 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -21,6 +24,16 @@ public class Robot extends TimedRobot {
      * for any
      * initialization code.
      */
+
+    private Spark leftMotor1 = new Spark(0);
+    private Spark leftMotor2 = new Spark(1);
+    private Spark rightMotor1 = new Spark(2);
+    private Spark rightMotor2 = new Spark(3);
+
+    private Joystick joy1 = new Joystick(0);
+
+    private double startTime;
+
     @Override
     public void robotInit() {
     }
@@ -31,11 +44,26 @@ public class Robot extends TimedRobot {
 
     @Override
     public void autonomousInit() {
+        startTime = Timer.getFPGATimestamp();
     }
 
     @Override
     public void autonomousPeriodic() {
+        double time = Timer.getFPGATimestamp();
+
+if(time < 3) {
+        leftMotor1.set(0.6);
+        leftMotor2.set(0.6);
+        rightMotor1.set(-0.6);
+        rightMotor2.set(-0.6);
+    } else {
+        leftMotor1.set(0);
+        leftMotor2.set(0);
+        rightMotor1.set(0);
+        rightMotor2.set(0);
     }
+}
+
 
     @Override
     public void teleopInit() {
@@ -43,6 +71,16 @@ public class Robot extends TimedRobot {
 
     @Override
     public void teleopPeriodic() {
+        double speed = -joy1.getRawAxis(1) * 0.6;
+        double turn = joy1.getRawAxis(4) * 0.3;
+
+        double left = speed + turn;
+        double right = speed - turn;
+        
+        leftMotor1.set(left);
+        leftMotor2.set(left);
+        rightMotor1.set(-right);
+        rightMotor2.set(-right);
     }
 
     @Override
