448a9a657c135952d666097e15a8c70f937c0dec
tanuj wadhawan <tanujwadhawan08@gmail.com>
2024-10-30 20:47:37 -0500

    Starting Over

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,43 +5,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 
 public class Robot extends TimedRobot {
-    private final PWMSparkMax left_drive = new PWMSparkMax(0);
-    private final PWMSparkMax right_drive = new PWMSparkMax(1);
-    private final DifferentialDrive robot_drive = new DifferentialDrive(left_drive::set, right_drive::set);
-    private final XboxController controller = new XboxController(0);
-    private final Timer timer = new Timer();
-
-    @Override
-    public void robotInit() {
-    }
-
-    @Override
-    public void robotPeriodic() {
-    }
-
-    @Override
-    public void autonomousInit() {
-        timer.reset();
-    }
-
-    @Override
-    public void autonomousPeriodic() {
-        if (timer.get() < 2.0) {
-            robot_drive.arcadeDrive(0.5, 0.0, false);
-
-        } else {
-            robot_drive.stopMotor();
-        }
-    }
-
-    @Override
-    public void teleopPeriodic() {
-        robot_drive.arcadeDrive(controller.getLeftY(), controller.getRightY(), true);
-    }
+    
 }
