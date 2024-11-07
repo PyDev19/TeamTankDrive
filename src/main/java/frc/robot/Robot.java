ae941656fe77758f5f208e6a52e72a254e161528
Keshav Sharma <keshav0218sharma@gmail.com>
2024-11-06 19:54:27 -0600

    Cool Important Changes(Actually doing it)

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,19 +1,74 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.motorcontrol.PWMTalonSRX;
 
 public class Robot extends TimedRobot {
 
-    @Override
-    public void autonomousInit() {
-        
-    }
+  private PWMTalonSRX leftMotor1 = new PWMTalonSRX(0);
+  private PWMTalonSRX leftMotor2 = new PWMTalonSRX(1);
+  private PWMTalonSRX rightMotor1 = new PWMTalonSRX(2);
+  private PWMTalonSRX rightMotor2 = new PWMTalonSRX(3);
 
-    @Override
-    public void autonomousPeriodic() {
-    }
+  private Joystick joy1 = new Joystick(0);
 
-    @Override
-    public void teleopPeriodic() {
-    }
+  @Override
+  public void robotInit() {
+
+  }
+
+  @Override
+  public void robotPeriodic() {
+
+  }
+
+  @Override
+  public void teleopInit() {
+
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    double speed = -joy1.getRawAxis(1) * 0.6;
+    double turn = joy1.getRawAxis(4) * 0.3;
+
+    double left = speed + turn;
+    double right = speed - turn;
+
+    leftMotor1.set(left);
+    leftMotor2.set(left);
+    rightMotor1.set(-right);
+    rightMotor2.set(-right);
+  }
+
+  @Override
+  public void disabledInit() {
+
+  }
+
+  @Override
+  public void disabledPeriodic() {
+
+  }
+
+  @Override
+  public void testInit() {
+
+  }
+
+  @Override
+  public void testPeriodic() {
+
+  }
+
+  @Override
+  public void simulationInit() {
+
+  }
+
+  @Override
+  public void simulationPeriodic() {
+
+  }
 }
